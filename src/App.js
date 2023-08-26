ab7a988679a00b3ea6220eb0426c7909ad577937
pifragile <pigu@bluewin.ch>
2023-08-26 13:32:12 +0200

    init teaser

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 //import "terminal.css";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { React, useState, createContext } from "react";
-import Home from "./components/Home";
-import TokenDetail from "./components/TokenDetail";
-import User from "./components/User";
-import MarketPlace from "./components/Marketplace";
-import About from "./components/About";
-
-import { WalletContext, beaconWallet } from "./lib/wallet";
+import { React, createContext } from "react";
 export const ModeContext = createContext(0);
 export const SeriesContext = createContext([]);
 
 function App() {
-    const [wallet] = useState(beaconWallet);
     return (
-        <WalletContext.Provider value={wallet}>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route
-                        path="/token-detail/:contract/:tokenId"
-                        element={<TokenDetail />}
-                    />
-                    <Route path="/user/:address" element={<User />} />
-                    <Route path="/marketplace" element={<MarketPlace />} />
-                </Routes>
-            </div>
-        </WalletContext.Provider>
+        <div
+            className="App"
+            style={{
+                background: "black",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh",
+                hwodtheight: "100vw",
+            }}
+        >
+            <img
+                src="/davinci.jpg"
+                style={{
+                    height: "min(500px, 100vw)"
+                }}
+            />
+        </div>
     );
 }
 
